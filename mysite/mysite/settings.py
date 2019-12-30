347efdc931c4c583bac1b006445b67bfa422250d
ANDRE <dreykim4@gmail.com>
2019-12-30 21:25:30 +0300

    Polls Site, Django website.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g#k!yd!r=-u+1(skcdkw)yio$17hj@j6^9ro*hu)1r5b&p#5d1'
+#SECRET_KEY = 'g#k!yd!r=-u+1(skcdkw)yio$17hj@j6^9ro*hu)1r5b&p#5d1'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','g#k!yd!r=-u+1(skcdkw)yio$17hj@j6^9ro*hu)1r5b&p#5d1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
